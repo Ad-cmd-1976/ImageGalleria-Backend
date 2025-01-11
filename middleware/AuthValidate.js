b6fc4a3538f6b88e5d18fa4a5407371611ceaa0a
Ad-cmd-1976 <adityavishwakarma9025@gmail.com>
2025-01-11 19:32:54 +0530

    okayd

--- middleware/AuthValidate.js
+++ middleware/AuthValidate.js
@@ -15,7 +15,7 @@ const signupValidation=(req,res,next)=>{
 const loginValidation=(req,res,next)=>{
     const schema=joi.object({
         email:joi.string().min(3).max(100).required(),
-        password:joi.string().min(8).max(100).required()
+        password:joi.string().min(4).max(100).required()
     })
     const {error}=schema.validate(req.body);
     if(error){
